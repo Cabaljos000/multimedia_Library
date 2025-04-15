1f5e8b234c53d4ce9da307ace3357e05b159e146
mhdz6989 <123098192+mhdz6989@users.noreply.github.com>
2025-04-15 17:09:04 +0000

    changes

--- db/migrate/20250415044658_add_publication_year_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddPublicationYearToBooks < ActiveRecord::Migration[8.0]
-  def change
-    add_column :books, :publication_year, :integer
-  end
-end
--- db/migrate/20250415045855_add_description_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToBooks < ActiveRecord::Migration[8.0]
-  def change
-    add_column :books, :description, :text
-  end
-end
